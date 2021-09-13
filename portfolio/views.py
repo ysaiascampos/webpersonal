3c5f33781946ca33db5443d47649b3d3f5e310b4
ysaiascampos <ysaiascampos@gmail.com>
2021-09-12 21:41:06 -0300

    hasta portafolio

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Proyect
 
 # Create your views here.
+
+def portfolio(request):
+    proyects = Proyect.objects.all() 
+    return render(request,"portfolio/portfolio.html", {'proyects':proyects})
