73e88baf82c44d1bbb9b3772f94482eddd18e6b7
ysaiascampos <ysaiascampos@gmail.com>
2021-09-13 21:24:22 -0300

    terminado

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.db.models.fields import URLField
 
 # Create your models here.
 class Proyect(models.Model):
     title = models.CharField(max_length=200, verbose_name = "Titulo")
     description = models.TextField(verbose_name = "Descripción")
     image = models.ImageField(verbose_name = "Imagen", upload_to="proyects")
+    link = models.URLField(verbose_name = "Direccion Web", null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, verbose_name = "Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name = "Fecha de Edición")
 
